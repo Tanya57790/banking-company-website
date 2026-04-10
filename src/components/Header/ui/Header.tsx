6956a088479fd1054fefbd3faf15100110c58a36
Tatyana Makhortova <tmahortova67@gmail.com>
2026-04-11 03:02:30 +0500

    feat(ui): add responsive Header component
    - Use handler state props for responsive Menu
    - Toggle hidden body for mobile Menu
    - ARIA: add role="navigation" for nav in header

---
+++ src/components/Header/ui/Header.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Menu, MobileMenu } from "@/components/Menu";
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handler = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    document.body.classList.toggle("overflow-hidden", isOpen);
+    return () => document.body.classList.remove("overflow-hidden");
+  }, [isOpen]);
+
+  return (
+    <header className="relative w-full mt-10 xl:mt-7.5 2xl:mt-12.5 z-10 border border-grey-150 bg-grey-110 rounded-[100px]">
+      <nav
+        role="navigation"
+        className="flex items-center justify-between py-3.5 pr-3.5 pl-6 mx-auto max-w-none w-full"
+      >
+        <Menu handler={handler} />
+        <MobileMenu handler={handler} isOpen={isOpen} />
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
