211ac3a4bfeb019397b9defcb96ad65a538ddf5d
Tatyana Makhortova <tmahortova67@gmail.com>
2026-05-13 23:56:55 +0500

    feat(ui): add SlideButton component
    - Type-safe: variantButton, isDesktop
    - Accessibility button: aria-label
    - Accessibility SVG Inline: role="img", aria-label, aria-hidden="true"

---
+++ src/components/SlideButton/ui/SlideButton.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useSwiper } from "swiper/react";
+import { arrowLeft, arrowRight } from "./data";
+
+type VariantButton = "prev" | "next";
+
+interface SlideButtonProps {
+  variantButton: VariantButton;
+  isDesktop?: boolean;
+}
+
+const SlideButton = ({ variantButton, isDesktop }: SlideButtonProps) => {
+  const swiper = useSwiper();
+  const arrow = variantButton === "prev" ? "Left" : "Right";
+
+  return (
+    <button
+      aria-label={variantButton === "prev" ? "Previous slide" : "Next slide"}
+      onClick={() =>
+        variantButton === "prev" ? swiper.slidePrev() : swiper.slideNext()
+      }
+      className={`${
+        variantButton === "prev" ? "swiper-button-prev" : "swiper-button-next"
+      } absolute cursor-pointer ${
+        isDesktop ? "hidden! md:flex!" : "flex"
+      } text-transparent! justify-center items-center px-2.75 py-3.75
+      w-13 h-13 xl:w-15.5 xl:h-15.5 bg-grey-150 rounded-[100px] z-20! transition-all duration-200 ease-in-out`}
+    >
+      <svg
+        role="img"
+        aria-label={`Arrow ${arrow}`}
+        aria-hidden="true"
+        width="17"
+        height="16"
+        viewBox="0 0 17 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="stroke-primary-600 active:stroke-primary-700 w-3.75 h-3.5 xl:w-4.5 xl:h-4.25"
+      >
+        <path
+          d={variantButton === "prev" ? arrowLeft : arrowRight}
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </button>
+  );
+};
+
+export default SlideButton;
---
+++ src/components/SlideButton/ui/data.ts
@@ -0,0 +1,3 @@
+export const arrowLeft = "M15.3333 7.75H0.75M7.75 14.75L0.75 7.75L7.75 0.75";
+export const arrowRight =
+  "M0.7487 7.75H15.332M8.33203 14.75L15.332 7.75L8.33203 0.75";
