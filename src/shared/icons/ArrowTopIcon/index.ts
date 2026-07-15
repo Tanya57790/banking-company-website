fdd60e70dcc588f988bd7c266052aca2818abb68
Tatyana Makhortova <tmahortova67@gmail.com>
2026-07-15 23:56:55 +0500

    feat(ui): add ArrowTopIcon component

---
+++ src/shared/icons/ArrowTopIcon/index.ts
@@ -0,0 +1,1 @@
+export { default as ArrowTopIcon } from "./ui/ArrowTopIcon";
---
+++ src/shared/icons/ArrowTopIcon/ui/ArrowTopIcon.tsx
@@ -0,0 +1,23 @@
+const ArrowTopIcon = () => {
+  return (
+    <svg
+      aria-hidden="true"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-6 h-6 2xl:w-8.5 2xl:h-8.5"
+    >
+      <path
+        d="M6.00005 19L19 5.99996M6.52005 5.99996H19V18.48"
+        className="stroke-primary-600"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ArrowTopIcon;
