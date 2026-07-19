20da4d33177ddfd22a4bf2ac8363e38cd0b94690
Tatyana Makhortova <tmahortova67@gmail.com>
2026-07-19 18:07:18 +0500

    feat(ui): add ArrowBottomIcon component

---
+++ src/shared/icons/ArrowBottomIcon/index.ts
@@ -0,0 +1,1 @@
+export { default as ArrowBottomIcon } from "./ui/ArrowBottomIcon";
---
+++ src/shared/icons/ArrowBottomIcon/ui/ArrowBottomIcon.tsx
@@ -0,0 +1,21 @@
+const ArrowBottomIcon = () => {
+  return (
+    <svg
+      aria-hidden="true"
+      width="11"
+      height="7"
+      viewBox="0 0 11 7"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-2.75 h-1.75 2xl:w-[5.5px] 2xl:h-2.75"
+    >
+      <path
+        d="M9.53027 0.530273L5.03027 5.03027L0.530273 0.530274"
+        className="stroke-white-absolute"
+        strokeWidth="1.5"
+      />
+    </svg>
+  );
+};
+
+export default ArrowBottomIcon;
