9ae3aec3f4af6059a1c0b3522db2dad5bf9286f4
Tatyana Makhortova <tmahortova67@gmail.com>
2026-07-22 14:58:59 +0500

    test(OurFeaturesSection): add button coverage
    Test button enabled, focus, and text changes on click.

---
+++ src/components/OurFeaturesSection/ui/OurFeaturesSection.test.tsx
@@ -0,0 +1,54 @@
+import { afterEach, describe, beforeEach, it, expect } from "vitest";
+import { cleanup, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import OurFeaturesSection from "./OurFeaturesSection";
+
+afterEach(() => {
+  cleanup();
+});
+
+describe("button enabled, focusable, and text changes on click", () => {
+  beforeEach(() => {
+    render(<OurFeaturesSection />);
+  });
+
+  it("first button is focusable and enabled", async () => {
+    const user = userEvent.setup();
+    const button = screen.getByRole("button", { name: /online banking/i });
+
+    expect(button).toBeInTheDocument();
+    await user.tab();
+    expect(button).toHaveFocus();
+    expect(button).toBeEnabled();
+  });
+
+  it("is financial tools text", async () => {
+    const user = userEvent.setup();
+    const button = screen.getByRole("button", { name: /financial tools/i });
+
+    expect(button).toBeInTheDocument();
+    await user.click(button);
+
+    const newText = await screen.findByText(/smart budgeting and insights/i);
+
+    await waitFor(() => {
+      expect(screen.queryByText(/account access/i)).not.toBeInTheDocument();
+    });
+    expect(newText).toBeInTheDocument();
+  });
+
+  it("is customer support text", async () => {
+    const user = userEvent.setup();
+    const button = screen.getByRole("button", { name: /customer support/i });
+
+    expect(button).toBeInTheDocument();
+    await user.click(button);
+
+    const newText = await screen.findByText(/responsive support team/i);
+
+    await waitFor(() => {
+      expect(screen.queryByText(/account access/i)).not.toBeInTheDocument();
+    });
+    expect(newText).toBeInTheDocument();
+  });
+});
