e48b112240040eaba9caa0a373b8cca01b28382c
Tatyana Makhortova <tmahortova67@gmail.com>
2026-07-25 21:09:07 +0500

    test(tests): add resizeWindow helper

---
+++ src/tests/helpers/resizeWindow.ts
@@ -0,0 +1,8 @@
+export const resizeWindow = (width: number) => {
+  Object.defineProperty(window, "innerWidth", {
+    writable: true,
+    configurable: true,
+    value: width,
+  });
+  window.dispatchEvent(new Event("resize"));
+};
