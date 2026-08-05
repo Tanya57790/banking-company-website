dd1176e0d397924864e7a760a7cf1a66ee74f196
Tatyana Makhortova <tmahortova67@gmail.com>
2026-08-06 00:02:45 +0500

    refactor(RegisterForm): switch form to new schema

--- src/forms/RegisterForm/ui/RegisterForm.tsx
+++ src/forms/RegisterForm/ui/RegisterForm.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { useTogglePassword, useRegisterForm } from "@/hooks";
-import { InferSchema } from "@/lib/schema";
+import { type SignUpSchema } from "@/lib/schema";
 import { EyePassword } from "@/components/EyePassword";
 import { AuthButtons } from "@/components/AuthButtons";
 import { login } from "@/components/Nav/ui/data";
@@ -10,7 +10,8 @@ import { inputs } from "./data";
 
 const Form = () => {
   const { isOpenEye, inputType, togglePassword } = useTogglePassword();
-  const { register, reg, onSubmit } = useRegisterForm();
+  const { signUpReg, onSubmitSignUp } = useRegisterForm();
+  const { register } = signUpReg;
 
   return (
     <div>
@@ -24,21 +25,21 @@ const Form = () => {
         </p>
       </div>
       <form
-        onSubmit={reg.handleSubmit(onSubmit)}
+        onSubmit={signUpReg.handleSubmit(onSubmitSignUp)}
         method="POST"
         className="relative w-full lg:w-166 2xl:w-256.5 m-auto z-2"
       >
         <div className="sm:grid sm:grid-cols-[1fr_1fr] sm:gap-[30px_24px] sm:justify-items-center">
           {inputs.map((input) => {
-            const fieldName = input.name as keyof InferSchema;
-            const error = reg.errors[fieldName];
+            const fieldName = input.name as keyof SignUpSchema;
+            const error = signUpReg.errors[fieldName];
 
             return (
               <p key={input.id} className="relative flex flex-col pb-6 sm:pb-0">
                 <input
                   id={`${input.id}-${input.type}-input`}
                   type={input.name === "password" ? inputType : input.type}
-                  {...register(`${input.name as keyof InferSchema}`)}
+                  {...register(`${input.name as keyof SignUpSchema}`)}
                   placeholder={input.placeholder}
                   autoComplete={input.autocomplete}
                   className="text-grey-350 p-4 xl:p-5 2xl:p-6 font-lexend font-light text-[16px] 2xl:text-lg lg:w-[320px] lg:h-16 2xl:w-124.5 2xl:h-18.75 bg-grey-110 rounded-[88px] focus:outline-none"
@@ -60,7 +61,7 @@ const Form = () => {
             <input
               type="submit"
               className="flex justify-center items-center font-lexend text-sm 2xl:text-lg w-full h-10 sm:h-12.25 xl:w-101 xl:h-12.25 2xl:w-129.5 2xl:h-15.75 mt-6 mb-6 xl:mt-7.5 2xl:mt-10 bg-primary-600 cursor-pointer hover:bg-primary-800 active:bg-primary-600 transition-all duration-200 ease-in-out rounded-[82px]"
-              value={reg.isSubmitting ? "Submitting..." : "Sign Up"}
+              value={signUpReg.isSubmitting ? "Submitting..." : "Sign Up"}
             />
             <Link
               href={login.href}
