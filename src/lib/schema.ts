81b975ac38151a9bbce74f79f81c913f783c5015
Tatyana Makhortova <tmahortova67@gmail.com>
2026-08-05 23:42:50 +0500

    chore(lib): separate login and register schemas

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,6 +1,6 @@
 import * as z from "zod";
 
-export const schema = z.object({
+export const signUpSchema = z.object({
   firstName: z
     .string()
     .min(1, "Required field")
@@ -38,4 +38,33 @@ export const schema = z.object({
     ),
 });
 
-export type InferSchema = z.infer<typeof schema>;
+export const signInSchema = z.object({
+  email: z
+    .string()
+    .max(320, "Maximum length is 320 characters")
+    .min(1, "Required field")
+    .refine((email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email), {
+      message: "Email is not valid",
+    }),
+  password: z
+    .string()
+    .min(1, "Required field")
+    .min(8, "Password must be at least 8 characters")
+    .max(20, "Password must be less than 20 characters")
+    .refine(
+      (val) => /[A-Z]/.test(val),
+      "Must contain at least one uppercase letter"
+    )
+    .refine(
+      (val) => /[a-z]/.test(val),
+      "Must contain at least one lowercase letter"
+    )
+    .refine((val) => /[0-9]/.test(val), "Must contain at least one number")
+    .refine(
+      (val) => /[!@#$%^&*]/.test(val),
+      "Must contain at least one special character"
+    ),
+});
+
+export type SignUpSchema = z.infer<typeof signUpSchema>;
+export type SignInSchema = z.infer<typeof signInSchema>;
