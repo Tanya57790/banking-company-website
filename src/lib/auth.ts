34329e6b63bdc55f9afa5c7fd891ac1d7bb579f1
Tatyana Makhortova <tmahortova67@gmail.com>
2026-08-17 21:34:47 +0500

    refactor(lib): simplify auth.ts and add test plugin
    - Remove redundant getClientIp
    - Add test plugin for Vitest coverage
    - Add additional fields for user model
    - Replace void with await to catch errors in try/catch.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,7 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma";
 import { sendEmail } from "./email";
 import { hashPassword, verifyPassword } from "./password";
+import { testUtils } from "better-auth/plugins";
 
 export interface TypedUser {
   id: string;
@@ -11,17 +12,28 @@ export interface TypedUser {
   lastName?: string;
 }
 
-const getClientIp = (request: Request): string => {
-  if (!request) return "unknown";
-  return (
-    request.headers.get("x-forwarded-for")?.split(",")[0]?.trim() || "unknown"
-  );
-};
-
 export const auth = betterAuth({
+  plugins: [...(process.env.NODE_ENV === "test" ? [testUtils()] : [])],
+  baseURL: process.env.BETTER_AUTH_URL! ?? "http://localhost:3000",
   database: prismaAdapter(prisma, {
     provider: "mysql",
   }),
+  user: {
+    additionalFields: {
+      firstName: {
+        type: "string",
+        required: true,
+        input: true,
+      },
+      lastName: {
+        type: "string",
+        required: true,
+        input: true,
+      },
+      role: { type: "string", required: false, input: false },
+      balance: { type: "number", required: false, input: false },
+    },
+  },
   emailAndPassword: {
     enabled: true,
     requireEmailVerification: true,
@@ -29,32 +41,72 @@ export const auth = betterAuth({
       hash: hashPassword,
       verify: verifyPassword,
     },
-    sendResetPassword: async ({ user, url, token }, request) => {
-      const ip = request ? getClientIp(request) : "unknown";
-      const resetUrl = `${url}?token=${token}&ip=${ip}`;
-      void sendEmail({
-        to: user.email,
-        subject: "Reset your password",
-        text: `Click the link to reset your password: ${resetUrl}`,
-      });
+    sendResetPassword: async ({ user, url, token }) => {
+      try {
+        console.log("=== Attempt to send email ===", user.email);
+        const verifyUrl = `${url}?token=${token}&redirect=/auth/login`;
+
+        await sendEmail({
+          to: user.email,
+          subject: "Reset your password",
+          text: `Click the link to reset your password: ${verifyUrl}`,
+        })
+          .then(() => console.log(`Email sent to ${user.email}`))
+          .catch((err) => console.error("Background sending error:", err));
+        console.log("=== Email sent successfully ===");
+      } catch (error) {
+        console.error("=== Nodemailer critical error ===");
+        console.error(error);
+
+        throw new Error(
+          "Failed to send reset password email due to SMTP misconfiguration."
+        );
+      }
     },
   },
   emailVerification: {
-    sendVerificationEmail: async ({ user, url, token }, request) => {
-      const ip = request ? getClientIp(request) : "unknown";
-      const verifyUrl = `${url}?token=${token}&ip=${ip}`;
-      void sendEmail({
-        to: user.email,
-        subject: "Verify your email address",
-        text: `Click the link to verify your email: ${verifyUrl}`,
-      });
+    sendOnSignUp: true,
+    autoSignInAfterVerification: false,
+    sendVerificationEmail: async ({ user, url, token }) => {
+      try {
+        console.log("=== Attempt to send email ===", user.email);
+        const verifyUrl = `${url}?token=${token}&redirect=/auth/login`;
+
+        sendEmail({
+          to: user.email,
+          subject: "Verify your email address",
+          text: `Click the link to verify your email: ${verifyUrl}`,
+        })
+          .then(() => console.log(`Email sent to ${user.email}`))
+          .catch((err) => console.error("Background sending error:", err));
+        console.log("=== Email sent successfully ===");
+      } catch (error) {
+        console.error("=== Nodemailer critical error ===");
+        console.error(error);
+
+        throw new Error(
+          "Failed to send verification email due to SMTP misconfiguration."
+        );
+      }
     },
     onExistingUserSignUp: async ({ user }: { user: TypedUser }) => {
-      void sendEmail({
-        to: user.email,
-        subject: "Sign-up attempt with your email",
-        text: "Someone tried to create an account using your email address. If this was you, try signing in instead. If not, you can safely ignore this email.",
-      });
+      try {
+        console.log("=== Attempt to send email ===", user.email);
+        await sendEmail({
+          to: user.email,
+          subject: "Sign-up attempt with your email",
+          text: "Someone tried to create an account using your email address. If this was you, try signing in instead. If not, you can safely ignore this email.",
+        })
+          .then(() => console.log(`Email sent to ${user.email}`))
+          .catch((err) => console.error("Background sending error:", err));
+      } catch (error) {
+        console.error("=== Nodemailer critical error ===");
+        console.error(error);
+
+        throw new Error(
+          "Failed to send email to verify registration of existing user due to SMTP misconfiguration."
+        );
+      }
     },
   },
   socialProviders: {
