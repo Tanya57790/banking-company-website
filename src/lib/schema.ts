dede425d37939774106036b99e8d0b878f57fe19
Tatyana Makhortova <tmahortova67@gmail.com>
2026-08-20 22:59:09 +0500

    refactor(schema): split email and password schema
    - Reuse email and password schema
    - Add schemas for ForgotPasswordForm and ResetPasswordForm

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,5 +1,36 @@
 import * as z from "zod";
 
+const emailSchema = {
+  email: z
+    .string()
+    .max(320, "Maximum length is 320 characters")
+    .min(1, "Required field")
+    .refine((email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email), {
+      message: "Email is not valid",
+    }),
+};
+
+const passwordSchema = {
+  password: z
+    .string()
+    .min(1, "Required field")
+    .min(8, "Password must be at least 8 characters")
+    .max(20, "Password must be less than 20 characters")
+    .refine(
+      (val) => /[A-Z]/.test(val),
+      "Must contain at least one uppercase letter"
+    )
+    .refine(
+      (val) => /[a-z]/.test(val),
+      "Must contain at least one lowercase letter"
+    )
+    .refine((val) => /[0-9]/.test(val), "Must contain at least one number")
+    .refine(
+      (val) => /[!@#$%^&*]/.test(val),
+      "Must contain at least one special character"
+    ),
+};
+
 export const signUpSchema = z.object({
   firstName: z
     .string()
@@ -11,60 +42,24 @@ export const signUpSchema = z.object({
     .min(1, "Required field")
     .min(2, "Last Name (at least 2 characters)")
     .max(50),
-  email: z
-    .string()
-    .max(320, "Maximum length is 320 characters")
-    .min(1, "Required field")
-    .refine((email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email), {
-      message: "Email is not valid",
-    }),
-  password: z
-    .string()
-    .min(1, "Required field")
-    .min(8, "Password must be at least 8 characters")
-    .max(20, "Password must be less than 20 characters")
-    .refine(
-      (val) => /[A-Z]/.test(val),
-      "Must contain at least one uppercase letter"
-    )
-    .refine(
-      (val) => /[a-z]/.test(val),
-      "Must contain at least one lowercase letter"
-    )
-    .refine((val) => /[0-9]/.test(val), "Must contain at least one number")
-    .refine(
-      (val) => /[!@#$%^&*]/.test(val),
-      "Must contain at least one special character"
-    ),
+  email: emailSchema.email,
+  password: passwordSchema.password,
 });
 
 export const signInSchema = z.object({
-  email: z
-    .string()
-    .max(320, "Maximum length is 320 characters")
-    .min(1, "Required field")
-    .refine((email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email), {
-      message: "Email is not valid",
-    }),
-  password: z
-    .string()
-    .min(1, "Required field")
-    .min(8, "Password must be at least 8 characters")
-    .max(20, "Password must be less than 20 characters")
-    .refine(
-      (val) => /[A-Z]/.test(val),
-      "Must contain at least one uppercase letter"
-    )
-    .refine(
-      (val) => /[a-z]/.test(val),
-      "Must contain at least one lowercase letter"
-    )
-    .refine((val) => /[0-9]/.test(val), "Must contain at least one number")
-    .refine(
-      (val) => /[!@#$%^&*]/.test(val),
-      "Must contain at least one special character"
-    ),
+  email: emailSchema.email,
+  password: passwordSchema.password,
+});
+
+export const resetPasswordSchema = z.object({
+  password: passwordSchema.password,
+});
+
+export const forgotPasswordSchema = z.object({
+  email: emailSchema.email,
 });
 
 export type SignUpSchema = z.infer<typeof signUpSchema>;
 export type SignInSchema = z.infer<typeof signInSchema>;
+export type ForgotPasswordSchema = z.infer<typeof forgotPasswordSchema>;
+export type ResetPasswordSchema = z.infer<typeof resetPasswordSchema>;
