4fdc2108fe200c44ed2249eefe91bada8fccf443
Tatyana Makhortova <tmahortova67@gmail.com>
2026-08-22 17:51:02 +0500

    refactor(ui): add session check to Nav and Menu

--- src/components/Menu/ui/Menu.tsx
+++ src/components/Menu/ui/Menu.tsx
@@ -2,24 +2,39 @@
 
 import Link from "next/link";
 import { Logo } from "@/shared/icons/Logo";
+import { ProfileIcon } from "@/shared/icons/ProfileIcon";
 import { Nav } from "@/components/Nav";
 import { login } from "@/components/Nav/ui/data";
+import { useSession } from "@/lib/auth-client";
 
 const Menu = () => {
+  const { data: session } = useSession();
+
   return (
     <div className="hidden md:flex items-center justify-between mx-auto max-w-none w-full">
       <Logo variant="header" />
       <Nav variant="header" />
-      <div className="hidden md:flex items-center justify-between w-43.25 lg:w-44 2xl:w-50">
-        <Link
-          href="/auth/register/"
-          className="flex items-center justify-center font-lexend text-sm 2xl:text-lg text-white-absolute w-19.5 h-10.25 hover:bg-grey-150 hover:rounded-[82px] transition-all duration-200 ease-in-out"
-        >
-          Sign Up
-        </Link>
-        <Link href={login.href} className={login.className}>
-          {login.name}
-        </Link>
+      <div className="hidden md:flex items-center justify-between w-max">
+        {session ? (
+          <Link
+            href="/account"
+            className="flex items-center font-lexend text-sm 2xl:text-lg text-white-absolute transition-all duration-200 ease-in-out justify-center md:h-10.25 2xl:h-12.75 mx-0.75 xl:mx-1.5 2xl:mx-1.75 md:px-2.5 hover:bg-grey-150 rounded-[82px]"
+          >
+            <ProfileIcon />
+          </Link>
+        ) : (
+          <>
+            <Link
+              href="/auth/register"
+              className="flex items-center justify-center font-lexend text-sm 2xl:text-lg text-white-absolute w-21.75 md:mr-2.75 md:w-19.5 2xl:mr-3.25 h-10.25 hover:bg-grey-150 hover:rounded-[82px] transition-all duration-200 ease-in-out"
+            >
+              Sign Up
+            </Link>
+            <Link href={login.href} className={login.className}>
+              {login.name}
+            </Link>
+          </>
+        )}
       </div>
     </div>
   );
--- src/components/Nav/ui/Nav.tsx
+++ src/components/Nav/ui/Nav.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Link from "next/link";
 import { login, navLinks } from "./data";
+import { useSession } from "@/lib/auth-client";
 
 type Variant = "header" | "footer" | "mobileMenu";
 
@@ -9,6 +12,8 @@ interface NavProps {
 }
 
 const Nav = ({ variant, isMobileMenu }: NavProps) => {
+  const { data: session } = useSession();
+
   return (
     <ul
       className={`${
@@ -47,19 +52,32 @@ const Nav = ({ variant, isMobileMenu }: NavProps) => {
       ))}
       {isMobileMenu && (
         <>
-          <li>
-            <Link
-              href="/auth/register/"
-              className="flex items-center justify-center font-lexend text-sm text-white-absolute w-21.75 h-10.25 mb-2.75 bg-grey-200 hover:bg-grey-150 transition-all duration-200 ease-in-out rounded-[82px]"
-            >
-              Sign Up
-            </Link>
-          </li>
-          <li>
-            <Link href={login.href} className={login.className}>
-              {login.name}
-            </Link>
-          </li>
+          {session ? (
+            <>
+              <Link
+                href="/account"
+                className="flex items-center font-lexend text-sm 2xl:text-lg text-white-absolute transition-all duration-200 ease-in-out w-19.5 h-10.25 mb-3.75 hover:text-primary-600 active:text-primary-700"
+              >
+                Account
+              </Link>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link
+                  href="/auth/register"
+                  className="flex items-center justify-center font-lexend text-sm text-white-absolute w-21.75 h-10.25 mb-2.75 bg-grey-200 hover:bg-grey-150 transition-all duration-200 ease-in-out rounded-[82px]"
+                >
+                  Sign Up
+                </Link>
+              </li>
+              <li>
+                <Link href={login.href} className={login.className}>
+                  {login.name}
+                </Link>
+              </li>
+            </>
+          )}
         </>
       )}
     </ul>
