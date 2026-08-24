3917123ca79f64cbbcfa3cf025a5f6c006f55876
Tatyana Makhortova <tmahortova67@gmail.com>
2026-08-24 23:15:21 +0500

    fix(app/auth): add Suspense wrapper for useSearchParams() in /auth/reset-password

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import { ResetPasswordForm } from "@/forms/ResetPasswordForm";
 import { FormBackground } from "@/components/FormBackground";
 
 export default function ResetPassword() {
   return (
-    <FormBackground>
-      <ResetPasswordForm />
-    </FormBackground>
+    <Suspense fallback={<div>Loading...</div>}>
+      <FormBackground>
+        <ResetPasswordForm />
+      </FormBackground>
+    </Suspense>
   );
 }
